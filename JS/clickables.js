4e859dcd972f05659f7b48a7ad367258ad623448
kcarlson33 <ktcarlson@gatech.edu>
2019-11-30 22:31:50 -0500

    Finished Javascript for Chosing and Option

--- JS/clickables.js
+++ JS/clickables.js
@@ -1,3 +1,5 @@
+var j1_deciding = false;
+
 function j1option1over() {
 	var imgBox = document.getElementById("imageSpace");
 	var newImgSrc = document.createElement("img");
@@ -5,7 +7,9 @@ function j1option1over() {
 	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
 	newImgSrc.style.width = '100%';
 	
-	imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
 }
 
 function j1option2over() {
@@ -18,7 +22,9 @@ function j1option2over() {
 	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
 	newImgSrc.style.width = '100%';
 	
-	imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
 }
 
 function j1out() {
@@ -31,5 +37,43 @@ function j1out() {
 	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
 	newImgSrc.style.width = '100%';
 	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j1options() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	
+	//Change the SRC to set the image
+	newImgSrc.setAttribute('src', '/Images/optionBG.jpg');
+	
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
 	imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	
+	var options = document.getElementById("options");
+	options.style.visibility = "visible";
+	
+	j1_deciding = true;
+}
+
+function j1option1() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>option 1</b>";
+	
+	j1out();
+}
+
+function j1option2() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>option 2</b>";
+	
+	j1out();
 }
