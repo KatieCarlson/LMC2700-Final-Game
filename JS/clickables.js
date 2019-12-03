e4c99ee0d112b8862dc24ea13485eeb3d37f0d11
lfischer27 <laurenfischer@gatech.edu>
2019-12-03 14:53:05 -0500

    added rest of journal pages

--- JS/clickables.js
+++ JS/clickables.js
@@ -12,12 +12,93 @@ function j1option1over() {
 	}
 }
 
+function j2option1over() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	newImgSrc.setAttribute('src', 'Images/interestedInMyBulletJournal.jpg');
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j3option1over() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	newImgSrc.setAttribute('src', 'Images/weOrderedSomeCoffee.jpg');
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j4option1over() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	newImgSrc.setAttribute('src', 'Images/messageEachOther.jpg');
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
 function j1out() {
 	var imgBox = document.getElementById("imageSpace");
 	var newImgSrc = document.createElement("img");
 	
 	//Change the SRC to set the image
 	newImgSrc.setAttribute('src', '../Images/cutSceneBeg1.jpg');
+	
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j2out() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	
+	//Change the SRC to set the image
+	newImgSrc.setAttribute('src', 'Images/recognizedMeAndGreetedMe.jpg');
+	
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j3out() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	
+	//Change the SRC to set the image
+	newImgSrc.setAttribute('src', 'Images/helpingHerWithItAfterClub.jpg');
+	
+	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
+	newImgSrc.style.width = '100%';
+	
+	if (!j1_deciding) {
+		imgBox.replaceChild(newImgSrc, imgBox.childNodes[1]);
+	}
+}
+
+function j4out() {
+	var imgBox = document.getElementById("imageSpace");
+	var newImgSrc = document.createElement("img");
+	
+	//Change the SRC to set the image
+	newImgSrc.setAttribute('src', 'Images/recognizedMeAndGreetedMe.jpg');
 	
 	newImgSrc.setAttribute('alt', 'ImageDescriptionHere');
 	newImgSrc.style.width = '100%';
@@ -61,6 +142,60 @@ function j1option2() {
 	document.getElementById("option").innerHTML = "<b> Why'd she smile at me? Could it be....?</b>";
 	
 	j1out();
+}
+
+function j2option1() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Must have really liked the journal.</b>";
+	
+	j2out();
+}
+
+function j2option2() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Could it be she was impressed by it...?</b>";
+	
+	j2out();
+}
+
+function j3option1() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Is this...a flag raising event? </b>";
+	
+	j3out();
+}
+
+function j3option2() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Does this mean future dates later? </b>";
+	
+	j3out();
+}
+
+function j4option1() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Without a doubt</b>";
+	
+	j4out();
+}
+
+function j4option2() {
+	j1_deciding = false;
+	options.style.visibility = "hidden";
+	
+	document.getElementById("option").innerHTML = "<b>Obviously</b>";
+	
+	j4out();
 }
 
 $(window).ready(function() {
